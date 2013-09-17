55dbbec6992f5be685abadc38dd524350cbad124
deep <system.grand@gmail.com>
2013-09-17 19:27:44 +0300

    more tests

---
+++ test/main/nested/A.hx
@@ -0,0 +1,15 @@
+package main.nested;
+import bindx.IBindable;
+
+/**
+ * ...
+ * @author deep <system.grand@gmail.com>
+ */
+class A implements IBindable {
+	
+	@bindable public var a:Int;
+	
+	public function new() {
+		
+	}
+}
